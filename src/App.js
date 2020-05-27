52840d50f1f6028da80000b70da18075e4ba3d6e
Jamie Carter <jc2820@hotmail.com>
2020-05-27 18:38:29 +0100

    add fake staff student login routes to work with

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 import './App.css';
 import Landing from './Landing.js';
 import Error from './Error.js';
+import Menu from './Menu.js';
 
 function App() {
+	const [user, setUser] = React.useState(null);
+
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route path="/" exact render={() => <Landing />} />
+				<Route path='/' exact render={() => <Landing setUser={setUser} />} />
+				<Route path='/menu' exact render={() => <Menu user={user} setUser={setUser} />} />
 				<Route component={Error} />
 			</Switch>
 		</BrowserRouter>
--- src/Landing.js
+++ src/Landing.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Landing = () => {
+const Landing = ({ setUser }) => {
 	return (
 		<nav>
-			<Link to='/menu' alt="Log in as participant"><button>Log in as Participant</button></Link>
-			<Link to='/menu' alt="Log in as organisation"><button>Log in as Staff</button></Link>
+			<Link to='/menu' alt="Log in as a participant"><button onClick={() => setUser("student")}>Log in as a Participant</button></Link>
+			<Link to='/menu' alt="Log in as a member of an organisation"><button onClick={() => setUser("staff")}>Log in as Staff</button></Link>
 		</nav>
 	)
 }
