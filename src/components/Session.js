ef4acf07a1a54f22d3eacc258352cab570d9ab45
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-05-28 16:27:41 +0100

    Add icons for resources ready for api call

--- src/components/Session.js
+++ src/components/Session.js
@@ -4,6 +4,7 @@ import React from 'react';
 const Session = () => {
 	return (
 		<>
+
 		<h1> This is the sessions page </h1>
 
 		</>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {ReactComponent as HomeSvg} from './home.svg';
-
+import './Home.css'
 const Home = () => {
 	return (
 		<HomeSvg className="home"/>
