6b10a0599bfb5e47e486b1c0740a19d3716d03aa
Jamie Carter <jc2820@hotmail.com>
2020-05-29 11:40:38 +0100

    add netlify lambda setup and first function

---
+++ functions/getSession/getSession.js
@@ -0,0 +1,43 @@
+const Airtable = require('airtable');
+
+exports.handler = (event, context, callback) => {
+  const { API_URL, API_CLIENT_ID, AIRTABLE_API_KEY } = process.env;
+
+  Airtable.configure({
+    endpointUrl: API_URL,
+    apiKey: AIRTABLE_API_KEY
+  });
+
+  const base = Airtable.base('appnkfsVctBYM5kva');
+  const data = [];
+
+  base('sessions')
+    .select({
+      maxRecords: 100,
+      view: "Grid view"
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+          data.push(record);
+        });
+        fetchNextPage();
+      },
+      function done(err) {
+        if (err) {
+          console.error(err);
+        } else {
+          const body = JSON.stringify({ records: data });
+          const response = {
+            statusCode: 200,
+            body: body,
+            headers: {
+              "content-type": "application/json",
+              "cache-control": "Cache-Control: max-age=60, public"
+            }
+          };
+          callback(null, response);
+        }
+      }
+    );
+};
