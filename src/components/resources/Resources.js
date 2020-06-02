d9038d338201d55468eec8e920ef040cbdc5c8e3
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-06-02 12:12:42 +0100

    Get resources data from airtable and render to resources page with map

--- src/components/resources/Resources.js
+++ src/components/resources/Resources.js
@@ -1,28 +1,40 @@
 import React from 'react';
-import {ReactComponent as Whatsapp } from './icons/whatsapp-link.svg';
-import {ReactComponent as Zoom } from './icons/zoom-link.svg';
-import {ReactComponent as Pdf } from './icons/pdf-link.svg';
-import {ReactComponent as Website } from './icons/website-link.svg';
-import {ReactComponent as Yt } from './icons/yt-link.svg';
-import {ReactComponent as Google} from './icons/google-link.svg'
+// import {ReactComponent as Whatsapp } from './icons/whatsapp-link.svg';
+// import {ReactComponent as Zoom } from './icons/zoom-link.svg';
+// import {ReactComponent as Pdf } from './icons/pdf-link.svg';
+// import {ReactComponent as Website } from './icons/website-link.svg';
+// import {ReactComponent as Yt } from './icons/yt-link.svg';
+// import {ReactComponent as Google} from './icons/google-link.svg'
 import PartNavbar from '../navbar/PartNavbar.js';
 import './Resources.css';
 
 const Resources = () => {
+const [resources, setResources] = React.useState(null);
+
+const getResources = async () => {
+	await (await fetch(`/.netlify/functions/getResources/getResources.js`))
+		.json()
+		.then((data) => setResources(data))
+		.catch(console.error);
+};
+
+React.useEffect(() => {
+	getResources();
+}, []);
+
+// console.log(resources);
+if (!resources) {
+	return <h1>Loading...</h1>;
+}
+
 	return (
 		<>
 		<PartNavbar />
-		<h1> List of Resources </h1>
-		<section className="resource-container">
-		<Whatsapp />
-		<Zoom />
-		<Yt />
-		<Google />
-		<Pdf />
-		<Website />
-		</section>
-		</>
-	)
-}
+
+	{resources.records.map(resource => (
+		<h1> {resource.fields.title} </h1>
+))}
+</>
+)}
 
 export default Resources;
