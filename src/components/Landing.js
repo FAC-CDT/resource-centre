96242c50777dd6b6c731ec3aa098e384c376505c
gregor <gregorlthomson@sky.com>
2020-06-03 15:58:06 +0100

    Add state in app.js, adjust routing based on state, add state for organisation and placeholder SVGs

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import ParticipantMenu from './menus/Participant-menu.js';
-import StaffMenu from './menus/Staff-menu.js';
+import React from "react";
+import { Link } from "react-router-dom";
+import ParticipantMenu from "./menus/Participant-menu.js";
+import StaffMenu from "./menus/Staff-menu.js";
 
-const Menu = ({ location }) => {
-	const userType = location.search.split('=')[1];
-	if (userType === 'staff') {
-		return (<StaffMenu />)
-	} else if (userType === 'participant'){
-		return (<ParticipantMenu />)
-	} else return (
-		<section>
-			<h1>401 error: Unauthorized</h1>
-			<Link to='/'><button>Back to the homepage</button></Link>
-		</section>
-	)
-}
+const Menu = ({ userRole }) => {
+  if (userRole === "staff") {
+    return <StaffMenu userRole={userRole} />;
+  } else if (userRole === "participant") {
+    return <ParticipantMenu userRole={userRole} />;
+  } else
+    return (
+      <section>
+        <h1>401 error: Unauthorized</h1>
+        <Link to="/">
+          <button>Back to the homepage</button>
+        </Link>
+      </section>
+    );
+};
 
 export default Menu;
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {ReactComponent as Participant } from './buttons/participant-login.svg'
-import {ReactComponent as Staff } from './buttons/staff-login.svg'
-import './Login.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Participant } from "./buttons/participant-login.svg";
+import { ReactComponent as Staff } from "./buttons/staff-login.svg";
+import { ReactComponent as PipLogo } from "./buttons/pip_logo.svg";
+import { ReactComponent as SafhLogo } from "./buttons/safh_logo.svg";
+import "./Login.css";
 
-
-const Landing = () => {
-	return (
-		<>
-		<h1> Welcome! </h1>
-		<nav>
-			<Link to='/landing?menu=participant' alt="Log in as a participant"><Participant /></Link>
-			<Link to='/landing?menu=staff' alt="Log in as a member of an organisation"><Staff /></Link>
-		</nav>
-		</>
-	)
-}
+const Landing = ({ userRole, setUserRole }) => {
+  return (
+    <>
+      <h1> Welcome! </h1>
+      <nav>
+        <Link to="/landing?menu=participant" alt="Log in as a participant">
+          <Participant
+            onClick={() => {
+              setUserRole("participant");
+            }}
+          />
+        </Link>
+        <Link
+          to="/landing?menu=staff"
+          alt="Log in as a member of an organisation"
+        >
+          <Staff
+            onClick={() => {
+              setUserRole("staff");
+            }}
+          />
+        </Link>
+        <PipLogo />
+        <SafhLogo />
+      </nav>
+    </>
+  );
+};
 
 export default Landing;
