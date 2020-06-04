d5f45e4ab1e1e0b4b5ade984abb3dfb45cbcc4da
Jamie Carter <jc2820@hotmail.com>
2020-06-04 14:47:31 +0100

    fix errors in airtable form embed

--- src/components/staff-options/AddResource.js
+++ src/components/staff-options/AddResource.js
@@ -7,10 +7,9 @@ const AddResource = () => {
       <Navbar />
       <iframe
 	      title="Add resource form"
-        class="airtable-embed"
+        className="airtable-embed"
         src="https://airtable.com/embed/shrHMAeoZxLhFovmG?backgroundColor=red"
-        frameborder="0"
-        onmousewheel=""
+        frameBorder="0"
         width="100%"
         height="533"
         style={{background: 'transparent', border: '1px solid #ccc'}}
