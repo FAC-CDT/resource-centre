e0d9e9cf22f664878b7912993e411f5348139f2e
Jamie Carter <jc2820@hotmail.com>
2020-06-05 11:01:40 +0100

    quick image fix for review meeting

--- src/components/login/OrganisationLogin.js
+++ src/components/login/OrganisationLogin.js
@@ -5,30 +5,33 @@ import SafhLogo from "./buttons/safh_logo.svg";
 import "./OrgLogin.css";
 
 const OrganisationLogin = ({ userRole, setUserOrganisation }) => {
-    return (
-        <>
-            <h1> Welcome! </h1>
-            <nav>
-                <Link to="/landing" alt="Log in as a participant">
-                    <img src={PipLogo} alt='PIP logo'
-                        onClick={() => {
-                            setUserOrganisation("pip");
-                        }}
-                    />
-                </Link>
-                <Link
-                    to="/landing"
-                    alt="Log in as a member of an organisation"
-                >
-                    <img src={SafhLogo} alt='safh logo'
-                        onClick={() => {
-                            setUserOrganisation("safh");
-                        }}
-                    />
-                </Link>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <h1> Welcome! </h1>
+      <nav>
+        <Link to="/landing" alt="Log in as a participant">
+          <img
+            src={PipLogo}
+            alt="PIP logo"
+            className="org-logo"
+            onClick={() => {
+              setUserOrganisation("pip");
+            }}
+          />
+        </Link>
+        <Link to="/landing" alt="Log in as a member of an organisation">
+          <img
+            src={SafhLogo}
+            alt="safh logo"
+            className="org-logo"
+            onClick={() => {
+              setUserOrganisation("safh");
+            }}
+          />
+        </Link>
+      </nav>
+    </>
+  );
 };
 
 export default OrganisationLogin;
